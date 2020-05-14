cb26188e9aa3ed556d1bbc8b7d919e462c7d73fc
liel titelbaum <liel.titelbaum1@gmail.com>
2020-05-15 01:37:34 +0300

    added timer, check if user won method and more fixes

--- memory_game/GameViewController.swift
+++ memory_game/GameViewController.swift
@@ -37,37 +37,31 @@ class GameViewController: UIViewController {
     private var pairedSuccessfully: Int = 0//how many cards are paired
     private var score = 0
     private var isPlaying = false
+    private var isUserWon = false
     private var imagesCounter = Array(repeating: 0, count: 8)
-    private var imageInPlace: [(image: UIImage,isOpen: Bool)] = []
+    private var imageInPlace: [(image: UIImage,isOpen: Bool)] = []//array of tuple of image and if the card is open -> the index represent the card tag(simulate its' location)
     private var cards: [UIButton] = []
-    private var counter: Int = 0
+    private var counter: Int = 0//pressing cards counter
+    private var timer = Timer()
     
     private let images = [#imageLiteral(resourceName: "ic_chicken") ,#imageLiteral(resourceName: "ic_burger") ,#imageLiteral(resourceName: "ic_fries") ,#imageLiteral(resourceName: "ic_broccoli") ,#imageLiteral(resourceName: "ic_chocolate") ,#imageLiteral(resourceName: "ic_sushi") ,#imageLiteral(resourceName: "ic_cake") ,#imageLiteral(resourceName: "ic_pizza")]
-    private let numOfMoves: Int = 25
+    private var numOfMoves: Int = 20
     private let back_card = #imageLiteral(resourceName: "icon_square")
     private let cardMatrixSize = 16 //4*4
     private let numOfImageDuplicate = 2
+    private let moves = 20
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         game_LBL_moves.text = String(numOfMoves)
-        
-        //        cards = [game_IMG_card0, game_IMG_card1, game_IMG_card2,
-        //                 game_IMG_card3, game_IMG_card4, game_IMG_card5, game_IMG_card6,
-        //                 game_IMG_card7, game_IMG_card8, game_IMG_card9, game_IMG_card10,
-        //                 game_IMG_card11, game_IMG_card12, game_IMG_card13, game_IMG_card14,
-        //                 game_IMG_card15]
-        
-        initCards()
-        
-        
+        game_LBL_timer.text = String(0) + " S"
+       
+
     }
     //functions/actions...
     
     func initCards() {
-        let images = [#imageLiteral(resourceName: "ic_chicken") ,#imageLiteral(resourceName: "ic_burger") ,#imageLiteral(resourceName: "ic_fries") ,#imageLiteral(resourceName: "ic_broccoli") ,#imageLiteral(resourceName: "ic_chocolate") ,#imageLiteral(resourceName: "ic_sushi") ,#imageLiteral(resourceName: "ic_cake") ,#imageLiteral(resourceName: "ic_pizza")]
-        
         //tag is the index i
         for i in 0..<cardMatrixSize {
             var rndCardImg = Int.random(in: 0 ..< images.count)
@@ -83,30 +77,33 @@ class GameViewController: UIViewController {
             imagesCounter[rndCardImg] += 1
             //               print("----- image counter ")
             //            print( imagesCounter[rndCardImg])
-            
-            
         }
     }
     
     func checkMatch(previous: UIButton, current: UIButton){
         if(imageInPlace[previous.tag].image == imageInPlace[current.tag].image && previous.tag != current.tag && imageInPlace[previous.tag].isOpen && imageInPlace[current.tag].isOpen) {
-            print(imageInPlace[previous.tag].image.description)
-            print(imageInPlace[current.tag].image.description)
+            print(imageInPlace[previous.tag].image.description)//delete
+            print(imageInPlace[current.tag].image.description)//delete
             
             pairedSuccessfully += 1
             previous.isEnabled = false
             current.isEnabled = false
             
-            print(pairedSuccessfully)
-            print("MATCH!! ")
+            print(pairedSuccessfully)//TODO: delete
+            print("MATCH!! ")//TODO: delte
             //TODO: add score later
             score += 10
             game_LBL_score.text = "Score: " + String(score)
         }
         else {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                self.closeCard(card: previous)
-                self.closeCard(card: current)
+            if(previous.tag != current.tag){
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    self.closeCard(card: previous)
+                    self.closeCard(card: current)
+                }
+            }
+            if(previous.tag == current.tag){
+                print("Clicked on the same card")
             }
             //TODO: need to count num of moves
         }
@@ -119,36 +116,78 @@ class GameViewController: UIViewController {
                           animations: nil, completion: nil)
     }
     
-    func closeCard(card: UIButton){
+    func closeCard(card: UIButton) {
         card.setImage(back_card, for: .normal)
         imageInPlace[card.tag].isOpen = false
         UIView.transition(with: card, duration: 0.5, options: .transitionFlipFromRight,
                           animations: nil, completion: nil)
     }
     
-    func gameOver() -> Bool{
-        if(pairedSuccessfully == (cardMatrixSize/numOfImageDuplicate)){
-            isPlaying = false
-            return true
+    func setTimer(on: Bool) {
+        var duration = 0
+        if(on) {
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+                duration += 1
+                self.game_LBL_timer.text = String(duration) + " S"
+            }
         }
-        return false
+        else {
+            timer.invalidate()
+        }
     }
     
     @IBAction func clickPlayGame(_ sender: UIButton) {
-        isPlaying = true
+        if(!isPlaying) {
+            initCards()
+            pairedSuccessfully = 0
+            numOfMoves = moves
+            setTimer(on: true)
+            isPlaying = true
+            game_BTN_play.isHidden = true
+        }
+        else {
+            isPlaying = false
+            game_BTN_play.isHidden = false
+        }
+    }
+    
+    func checkIfWinner() {
+        if(pairedSuccessfully == cardMatrixSize/numOfImageDuplicate) {//if the user matched all of the cards
+            setTimer(on: false)
+            game_BTN_play.setTitle("YOU WON!\nPlay again", for: .normal)
+            game_BTN_play.isHidden = false
+            isUserWon = true
+            isPlaying = false
+        }
+        else if(numOfMoves == 0){//if he lost -> ran out of moves
+            setTimer(on: false)
+            isUserWon = false
+            isPlaying = false
+            game_BTN_play.setTitle("YOU LOST\nPlay again", for: .normal)
+            game_BTN_play.isHidden = false
+        }
+        
     }
     @IBAction func button_clicked(_ sender: UIButton) {
-    
-        counter += 1 //amount of button clicks
-        print(counter)
-        openCard(card: sender)
         
-        if(counter % 2 == 0){
-            //TODO: need to handle moves
-            //TODO: handle with other cards open -> while 2 cards are open -> cant open new cards...
-            checkMatch(previous: previousButton, current: sender)
+        if(isPlaying) {
+            
+            counter += 1 //amount of button clicks
+            print(counter)
+            openCard(card: sender)
+            
+            if(counter % 2 == 0){
+                //TODO: need to handle moves
+                //TODO: handle with other cards open -> while 2 cards are open -> cant open new cards...
+                if(sender != previousButton) {
+                    numOfMoves -= 1
+                    game_LBL_moves.text = String(numOfMoves)
+                }
+                checkMatch(previous: previousButton, current: sender)
+            }
+            previousButton = sender
         }
-        previousButton = sender
+         checkIfWinner()
     }
     
 }
